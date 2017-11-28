22e9a35cf631fa732028245ed2d6d61ab852a5e9
Phuc Cao <hanhphuc296@gmail.com>
2017-11-28 14:33:20 -0500

    Removed urls

--- testing.py
+++ testing.py
@@ -1,8 +1,10 @@
 import json
+import re 
+
 data = json.load(open('nov1to5.json'))
 outputFile = open('output.txt', 'w')
 
 for item in data:
-    outputFile.write(item["text"].encode('utf-8') + " ")
+    outputFile.write(re.sub(r"http\S+", '', item["text"].encode('utf-8') + " ", flags=re.MULTILINE))
 
 outputFile.close()
