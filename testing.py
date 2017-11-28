cb31afb4f25e5db3c95b01ae90a1d23a175247b2
Calla <maracondo@aol.com>
2017-11-28 14:29:12 -0500

    Improved text creation

--- testing.py
+++ testing.py
@@ -3,6 +3,6 @@ data = json.load(open('nov1to5.json'))
 outputFile = open('output.txt', 'w')
 
 for item in data:
-    outputFile.write(item["text"].encode('utf-8'))
+    outputFile.write(item["text"].encode('utf-8') + " ")
 
 outputFile.close()
